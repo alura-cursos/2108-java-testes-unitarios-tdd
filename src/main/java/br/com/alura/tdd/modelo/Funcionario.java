8ef1243b7ddaafc523bac3f5a83cfdae66c4a0c6
Rodrigo Caneppele <r.caneppele@gmail.com>
2021-01-30 18:31:33 -0300

    projeto inicial

---
+++ src/main/java/br/com/alura/tdd/modelo/Funcionario.java
@@ -0,0 +1,30 @@
+package br.com.alura.tdd.modelo;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Funcionario {
+
+	private String nome;
+	private LocalDate dataAdmissao;
+	private BigDecimal salario;
+
+	public Funcionario(String nome, LocalDate dataAdmissao, BigDecimal salario) {
+		this.nome = nome;
+		this.dataAdmissao = dataAdmissao;
+		this.salario = salario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public LocalDate getDataAdmissao() {
+		return dataAdmissao;
+	}
+
+	public BigDecimal getSalario() {
+		return salario;
+	}
+
+}
---
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -0,0 +1,17 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
